1f38dffc3bd8a454627d77d9d67edfe9398f761c
Nasif Ahmed <nasif2ahmed@gmail.com>
2025-06-01 15:16:52 +0600

    fix deployment : remove unnecessary imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import AboutCard from "@/components/about-card";
 import ActivityCard from "@/components/activity-card";
-import ColumnContainer from "@/components/column-container";
 import ConsumingCard from "@/components/consuming-card";
 import Container from "@/components/container";
 import Footer from "@/components/footer";
 import HardwareCard from "@/components/hardware-card";
 import TechCard from "@/components/tech-card";
-import TestCard from "@/components/test-card";
 
 export default function Home() {
     return (
