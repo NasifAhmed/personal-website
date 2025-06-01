21a92b53664be4dc88b2b8f53577a4a855c6a1a0
Nasif Ahmed <nasif2ahmed@gmail.com>
2025-06-01 14:15:29 +0600

    Update rearrange homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AboutCard from "@/components/about-card";
 import ActivityCard from "@/components/activity-card";
 import ColumnContainer from "@/components/column-container";
+import ConsumingCard from "@/components/consuming-card";
 import Container from "@/components/container";
 import HardwareCard from "@/components/hardware-card";
 import TechCard from "@/components/tech-card";
@@ -10,11 +11,12 @@ export default function Home() {
         <Container>
             <ColumnContainer>
                 <AboutCard></AboutCard>
-                <ActivityCard />
-            </ColumnContainer>
-            <ColumnContainer>
                 <TechCard />
                 <HardwareCard />
+            </ColumnContainer>
+            <ColumnContainer>
+                <ActivityCard />
+                <ConsumingCard />
             </ColumnContainer>
         </Container>
     );
